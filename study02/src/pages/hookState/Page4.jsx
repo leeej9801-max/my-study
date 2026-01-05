52283a76dc488e52bc05425f7cc2f29b20d0b231
shellfolders <232290329+shellfolders@users.noreply.github.com>
2026-01-05 15:20:51 +0900

    page4 Input 함수 사용하기

--- study02/src/pages/hookState/Page4.jsx
+++ study02/src/pages/hookState/Page4.jsx
@@ -1,58 +1,79 @@
 import { useState } from "react"
 import { setStorage, getStorage, removeStorage } from "@utils/storage.js"
 
+const Input = props => {
+  return (
+    <div className="mb-3 mt-3">
+      <label htmlFor={props.id} className="form-label">{props.name}:</label>
+      <input type={props.type} className="form-control" 
+             id={props.id} placeholder={props.desc} 
+             name={props.id} required={props.required}
+             onChange={e=> props.onChange(e.target.value)}/>
+    </div>
+  )
+}
+const Gender = props => {
+  const event = e => props.checkBoolean(e.target.value)
+  return (
+    <div className="d-flex">
+      <div className="p-2 flex-fill">
+        <div className="form-check">
+        <input type="radio" className="form-check-input" id="gender1" name={props.id} value="1" checked={props.gender} onChange={event}/>남성
+        <label className="form-check-label" htmlFor="gender1"></label>
+      </div>
+      </div>
+      <div className="p-2 flex-fill">
+        <div className="form-check">
+        <input type="radio" className="form-check-input" id="gender2" name={props.id} value="2" checked={!props.gender} onChange={event}/>여성
+        <label className="form-check-label" htmlFor="gender2"></label>
+      </div>
+      </div>
+    </div>
+  )
+}
+const Buttons = props => {
+  return (
+    <div className="d-flex gap-2">
+      <div className="flex-fill d-grid">
+        <button type="submit" className="btn btn-primary">생성</button>
+      </div>
+      <div className="flex-fill d-grid">
+        <button type="button" className="btn btn-primary">취소</button>
+      </div>
+    </div>
+  )
+}
+
 const Page4 = () => {
-  const [data, setData] = useState({"name": "", "email": "", "pwd": "", "gender": true})
-  const checkBoolean = (name, value) => {
-    if(name === "gender") return value === "1" ? true : false
-    return value
-  }
-  const onChange = e => { 
-    const {name, value} = e.target
-    setData({...data, [name]: checkBoolean(name, value)})
-  }
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [pwd, setPwd] = useState("")
+  const [gender, setGender] = useState(true)
+  const checkBoolean = value => setGender(value === "1" ? true : false)
   const onSubmit = e => {
     e.preventDefault()
+    const data = {name, email, pwd, gender}
     console.log(data)
   }
+  const arr = [
+    {id: "name", name: "이름", type: "text", desc: "이름을 입력하세요.", 
+     required: true, onChange: setName},
+    {id: "email", name: "이메일", type: "email", desc: "이메일를 입력하세요.", 
+     required: true, onChange: setEmail},
+    {id: "pwd", name: "비밀번호", type: "password", desc: "비밀번호를 입력하세요.", 
+     required: false, onChange: setPwd}
+  ]
   return (
     <div className="container mt-3">
       <h1 className="display-1 text-center">사용자 등록</h1>
       <form onSubmit={onSubmit}>
-        <div className="mb-3 mt-3">
-          <label htmlFor="name" className="form-label">이름:</label>
-          <input type="text" className="form-control" id="name" placeholder="이름을 입력하세요." name="name" onChange={onChange}/>
-        </div>
-        <div className="mb-3 mt-3">
-          <label htmlFor="email" className="form-label">이메일:</label>
-          <input type="email" className="form-control" id="email" placeholder="이메일를 입력하세요." name="email" onChange={onChange}/>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="pwd" className="form-label">비밀번호:</label>
-          <input type="password" className="form-control" id="pwd" placeholder="비밀번호를 입력하세요." name="pwd" onChange={onChange}/>
-        </div>
-        <div className="d-flex">
-          <div className="p-2 flex-fill">
-            <div className="form-check">
-            <input type="radio" className="form-check-input" id="radio1" name="gender" value="1" checked={data.gender} onChange={onChange}/>남성
-            <label className="form-check-label" htmlFor="radio1"></label>
-          </div>
-          </div>
-          <div className="p-2 flex-fill">
-            <div className="form-check">
-            <input type="radio" className="form-check-input" id="radio2" name="gender" value="2" checked={!data.gender} onChange={onChange}/>여성
-            <label className="form-check-label" htmlFor="radio2"></label>
-          </div>
-          </div>
-        </div>
-        <div className="d-flex gap-2">
-          <div className="flex-fill d-grid">
-            <button type="submit" className="btn btn-primary">생성</button>
-          </div>
-          <div className="flex-fill d-grid">
-            <button type="button" className="btn btn-primary">취소</button>
-          </div>
-        </div>
+        {
+          arr.map((v, i) => <Input key={i} id={v.id} name={v.name}
+               type={v.type} desc={v.desc}
+               required={v.required} onChange={v.onChange} />)
+        }
+        <Gender gender={gender} checkBoolean={checkBoolean}/>
+        <Buttons />
       </form>
     </div>
   )
--- study02/src/pages/hookState/index.jsx
+++ study02/src/pages/hookState/index.jsx
@@ -4,6 +4,7 @@ import Page1 from "@pages/hookState/Page1.jsx"
 import Page2 from "@pages/hookState/Page2.jsx"
 import Page3 from "@pages/hookState/Page3.jsx"
 import Page4 from "@pages/hookState/Page4.jsx"
+import Page5 from "@pages/hookState/Page5.jsx"
 
 const Root = () => {
   const pages = [
@@ -11,6 +12,7 @@ const Root = () => {
     {"name": "Page2", "url": "2"},
     {"name": "Page3", "url": "3"},
     {"name": "Page4", "url": "4"},
+    {"name": "Page5", "url": "5"},
   ]
   return (
     <div className="container mt-3">
@@ -38,6 +40,7 @@ const HookState = () => {
       <Route path="3" element={<Page3 />} />
       <Route path="4" element={<Page4 />} />
       <Route path="4" element={<Page4 />} />
+      <Route path="5" element={<Page5 />} />
       <Route path="*" element={<Root />} />
     </Routes>
   )
