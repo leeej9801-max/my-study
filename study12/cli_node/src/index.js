22773fed36557529247c9830283bcb9e53ad7b5c
shellfolders <232290329+shellfolders@users.noreply.github.com>
2026-01-15 17:54:11 +0900

    문제 풀이

--- study12/cli_node/src/index.js
+++ study12/cli_node/src/index.js
@@ -1,10 +1,12 @@
-console.log("자바스크립트");
+// console.log("자바스크립트");
 
 import { Command } from 'commander';
-import { add, list } from './cmd.js';
+// import { add, list } from './cmd.js';
+import { list, insert, update, remove } from './words.js';
 
 const program = new Command();
 
+/*
 program
   .command('add')
   .argument('<a>')
@@ -16,5 +18,13 @@ program
   .command('list')
   .description('목록보기')
   .action(list);
+*/
+
+
+
+program.command('list').action(list);
+program.command('insert').argument('<word>').action(insert);
+program.command('update').argument('<id>').argument('<word>').action(update);
+program.command('delete').argument('<id>').action(remove);
 
 program.parse(process.argv);
