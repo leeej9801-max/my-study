c0759e3165998ea7958ab4385c62af6e60ecb63d
shellfolders <232290329+shellfolders@users.noreply.github.com>
2026-01-15 12:18:37 +0900

    파일 관리 CLI 프로그램 알아보기

--- study12/cli_node/src/index.js
+++ study12/cli_node/src/index.js
@@ -1,2 +1,20 @@
-console.log("자바스크립트")
+console.log("자바스크립트");
 
+import { Command } from 'commander';
+import { add, list } from './cmd.js';
+
+const program = new Command();
+
+program
+  .command('add')
+  .argument('<a>')
+  .argument('<b>')
+  .description('메모 추가')
+  .action(add);
+
+program
+  .command('list')
+  .description('목록보기')
+  .action(list);
+
+program.parse(process.argv);
