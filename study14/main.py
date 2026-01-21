c20c0c160e5399781a2a8429cc903bf90f2e1d66
shellfolders <232290329+shellfolders@users.noreply.github.com>
2026-01-21 16:46:05 +0900

    insert 및 commit

--- study14/main.py
+++ study14/main.py
@@ -48,10 +48,45 @@ def findAll(sql):
     print(f"MariaDB Error : {e}")
   return result
 
-sql = """
-    SELECT 1 as NO
-    UNION ALL
-    SELECT 2
+def save(sql):
+  result = False
+  try:
+    conn = getConn()
+    if conn:
+      cur = conn.cursor()
+      cur.execute(sql)
+      conn.commit()
+      cur.close()
+      conn.close()
+      result = True
+  except mariadb.Error as e:
+    print(f"MariaDB Error : {e}")
+  return result
+
+deptNo1 = "d001"
+deptNo2 = "d002"
+deptNo3 = "d009"
+arr = [deptNo1,deptNo2,deptNo3]
+sql = f"""
+    SELECT dept_no, COUNT(emp_no) AS cnt 
+      FROM edu.dept_emp 
+    WHERE dept_no IN { tuple(arr) }
+    GROUP BY dept_no
+    ORDER BY 2 DESC
     """
-print( findOne(sql) )
-print( findAll(sql) )
+# print(sql)
+# print( findOne(sql) )
+# print( findAll(sql) )
+
+sql1 = "SELECT * FROM edu.test"
+print( findAll(sql1) )
+
+name = "둘리"
+sql2 = f"""
+        INSERT INTO edu.test 
+          (`name`, `regDate`) 
+        VALUE 
+          ('{name}', NOW())
+      """
+print( save(sql2) )
+print( findAll(sql1) )
