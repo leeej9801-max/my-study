e0d1f69284e898eb819add52e8f037d9c7020b9d
shellfolders <232290329+shellfolders@users.noreply.github.com>
2026-01-28 09:32:12 +0900

    React와 FastAPI 프로젝트 생성

---
+++ study18/frontend/src/pages/App.jsx
@@ -0,0 +1,24 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { BrowserRouter, Routes, Route } from "react-router";
+import '@styles/App.css'
+import Home from '@pages/Home.jsx'
+import NotFound from '@pages/NotFound.jsx'
+
+const App = () => {
+  const paths = [
+    {path: "/", element: <Home />},
+    {path: "*", element: <NotFound />},
+  ]
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          { paths?.map((v, i) => <Route key={i} path={v.path} element={v.element} />) }
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
