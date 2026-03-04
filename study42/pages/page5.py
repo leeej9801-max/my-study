064fac184c90357e9a136b6c0519216fb4227502
shellfolders <232290329+shellfolders@users.noreply.github.com>
2026-03-04 16:23:12 +0900

    llm 이용한 번역

--- study42/pages/page5.py
+++ study42/pages/page5.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import trafilatura as tra
+import ollama
+import re
 
 st.set_page_config(
 	page_title="4. 뉴스기사 요약",
@@ -18,8 +20,26 @@ def extract_txt_image(url: str):
 # 예시 : https://www.koreaherald.com/article/10685727
 # 예시 : https://www.koreaherald.com/article/10686698
 if url := st.text_input("주소 입력", placeholder="URL을 입력하세요"):
-	text, image = extract_txt_image(url)
-	message = text
-	st.image(image)
-	st.markdown(message)
+  text, image = extract_txt_image(url)
+  message = ""
+  st.image(image)
+  if re.search('[ㄱ-ㅎㅏ-ㅣ가-힣]', text): 
+    st.markdown(message)
+  else:
+    message_placeholder = st.empty()
+    prompt = f"다음 영어 기사를 한글로 번역해주세요:\n {text}"
+    stream = ollama.chat(
+      model="gpt-oss:20b",
+      messages=[{"role": "user", "content": prompt}],
+      stream=True
+    )
+    full_response = ""
+    for chunk in stream:
+      content = chunk["message"]["content"]
+      full_response += content
+      message_placeholder.markdown(full_response + "▌")
+		# res = ollama.chat(model="gpt-oss:20b", messages=[{"role":"user","content":prompt}])
+		# st.markdown(res.message.content)
+    
+
 	
