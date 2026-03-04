a490fd33c9ac6dc8e03c476bd5c046fe75072a3e
shellfolders <232290329+shellfolders@users.noreply.github.com>
2026-03-04 12:35:30 +0900

    위키백과 수집

---
+++ study42/pages/page4.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+episode_links = [
+	"https://en.wikipedia.org/wiki/Demon_Slayer:_Kimetsu_no_Yaiba_season_1",
+	"https://en.wikipedia.org/wiki/Demon_Slayer:_Kimetsu_no_Yaiba_season_2",
+	"https://en.wikipedia.org/wiki/Demon_Slayer:_Kimetsu_no_Yaiba_season_3",
+	"https://en.wikipedia.org/wiki/Demon_Slayer:_Kimetsu_no_Yaiba_season_4",
+]
+options = ["Season1","Season2","Season3","Season4"]
