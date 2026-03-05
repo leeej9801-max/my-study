3c1228a9407ad011ba37ea15c13c413761daace6
shellfolders <232290329+shellfolders@users.noreply.github.com>
2026-03-05 11:18:27 +0900

    Yes24 & InterPark 수집

--- study43/pages/1_yes24.py
+++ study43/pages/1_yes24.py
@@ -21,34 +21,45 @@ weekNo = 1149
 sex = "A"
 viewMode = "thumb"
 
-url = (
-  f"{yes24}?"
-  f"categoryNumber={categoryNumber}&"
-  f"pageNumber={pageNumber}&"
-  f"pageSize={pageSize}&"
-  f"type={type}&"
-  f"saleYear={saleYear}&"
-  f"weekNo={weekNo}&"
-  f"sex={sex}&"
-  f"viewMode={viewMode}"
-)
-
 # 데이터 수집
 def getData():
   try:
-    url = ""
+    url = (
+      f"{yes24}?"
+      f"categoryNumber={categoryNumber}&"
+      f"pageNumber={pageNumber}&"
+      f"pageSize={pageSize}&"
+      f"type={type}&"
+      f"saleYear={saleYear}&"
+      f"weekNo={weekNo}&"
+      f"sex={sex}&"
+      f"viewMode={viewMode}"
+    )
     st.text(f"URL: {url}")
     res = get(url)
     if res.status_code == 200:
       st.text("yes24 국내도서 주별 베스트 수집 시작!")
       books = [] # { 도서명, 저자, 별점 }
+      soup = bs(res.text, "html.parser")
+      trs = soup.select("#yesBestList .itemUnit")
+      for item in trs:
+        title = item.select_one(".gd_name").get_text(strip=True)
+        author_span = item.select_one("span.authPub.info_auth")
+        author = author_span.select_one("a").get_text(strip=True)
+        star_span = item.select_one("span.rating_grade")
+        star = star_span.select_one("em.yes_b").get_text(strip=True)
+        books.append({ "title": title, "author": author, "star": star })
       tab1, tab2, tab3 = st.tabs(["HTML 데이터", "JSON 데이터", "DataFrame"])
       with tab1:
         st.text("html 출력")
+        st.html(trs)
       with tab2:
         st.text("JSON 출력")
+        json_string = json.dumps(books, ensure_ascii=False, indent=2)
+        st.json(body=json_string, expanded=True, width="stretch")
       with tab3:
         st.text("DataFrame 출력")
+        st.dataframe(pd.DataFrame(books))
   except Exception as e:
     return 0
   return 1
--- study43/pages/2_interpark.py
+++ study43/pages/2_interpark.py
@@ -24,22 +24,47 @@ drama_url = "https://tickets.interpark.com/contents/ranking?genre=DRAMA"
 # 전시 url
 exhibit_url = "https://tickets.interpark.com/contents/ranking?genre=EXHIBIT"
 
+musical_key = '@"/ranking","?period=D&page=1&pageSize=50&rankingTypes=MUSICAL",'
+
 # 데이터 수집
 def getData():
   try:
-    url = ""
+    url = musical_url
     st.text(f"URL: {url}")
     res = get(url)
     if res.status_code == 200:
       st.text("인터파크 티켓 수집 시작!")
       tickets = [] # { 장르, 티켓이름, 장소, 시작날짜, 종료날짜, 예매율 }
-      tab1, tab2, tab3 = st.tabs(["HTML 데이터", "JSON 데이터", "DataFrame"])
+      soup = bs(res.text, "html.parser")
+      items = soup.select("div.responsive-ranking-list_rankingItem__PuQPJ")
+      genre = "MUSICAL"
+      for item in items:
+        tName = item.select_one("li.responsive-ranking-list_goodsName__aHHGY").get_text(strip=True)
+        pName = item.select_one("li.responsive-ranking-list_placeName__9HN2O").get_text(strip=True)
+        tDate = item.select_one("div.responsive-ranking-list_dateWrap__jBu5n").get_text(strip=True)
+        tPercent = item.select_one("li.responsive-ranking-list_bookingPercent__7ppKT").get_text(strip=True)
+        tickets.append({ "genre": genre, "tName": tName, "pName": pName, "tDate": tDate, "tPercent": tPercent })
+      tab1, tab2, tab3, tab4 = st.tabs(["HTML 데이터", "json 데이터", "DataFrame", "API 데이터"])
       with tab1:
-        st.text("html 출력")
+        st.text("HTML 출력")
+        # st.html(items)
+        st.text(items)
       with tab2:
         st.text("JSON 출력")
+        # st.json(arr)
+        json_string = json.dumps(tickets, ensure_ascii=False, indent=2)
+        st.json(body=json_string, expanded=True, width="stretch")
       with tab3:
         st.text("DataFrame 출력")
+        st.dataframe(pd.DataFrame(tickets))
+      with tab4:
+        st.text("API 출력")
+        script_tag = soup.find('script', {'id': '__NEXT_DATA__'})
+        json_data = json.loads(script_tag.string)
+        st.json(json_data, expanded=False)
+        st.html("<hr/>")
+        st.text(f"{genre} 목록 출력")
+        st.json(json_data.get('props', {}).get('pageProps', {}).get('fallback', {}).get(musical_key, []), expanded=False)
   except Exception as e:
     return 0
   return 1
