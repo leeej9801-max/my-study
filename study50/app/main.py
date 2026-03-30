62fc9f6312a219e487f3cc8f4d674c239f41627b
shellfolders <232290329+shellfolders@users.noreply.github.com>
2026-03-30 14:32:24 +0900

    ollama 기본 테스트

--- study50/app/main.py
+++ study50/app/main.py
@@ -1,6 +1,44 @@
+import ollama
+
+# ---------------------------
+# Ollama LLM 호출 함수
+# ---------------------------
+def llm_call_structured(prompt: str, model: str = "gemma3:4b"):
+
+  # Ollama에 LLM 요청
+  response = ollama.chat(
+    model=model,
+    messages=[{"role": "user", "content": prompt}]
+  )
+
+  # 모델 응답 텍스트 추출
+  text = response["message"]["content"]
+
+  # JSON 파싱 시도
+  try:
+    print(text)
+  except Exception as e:
+    print(f"응답 오류: {e}")
+
+# ------------------------------------------------------
+# 메인 실행 함수
+# ------------------------------------------------------
 def main():
-    print("Hello from app!")
+  try:
+    # 프롬프트 설정
+    prompt = """
+      안녕하세요
+    """
 
+    # LLM 호출
+    llm_call_structured(prompt)
+  except Exception as e:
+    print(f"오류 발생: {e}")
+    return 1
+  return 0
 
+# ------------------------------------------------------
+# 프로그램 실행
+# ------------------------------------------------------
 if __name__ == "__main__":
-    main()
+  exit(main())
